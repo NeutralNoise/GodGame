88b8487b4b1755c0aa39a6b8035a9d0b2df99034
NeutralNoise <classicbrad24@gmail.com>
2019-06-06 16:06:13 +1000

    Added doxygen comments.

--- GodGame/GodGame/SDLEngine/RenderEngine/RenderEngine.h
+++ GodGame/GodGame/SDLEngine/RenderEngine/RenderEngine.h
@@ -5,11 +5,11 @@
 
 struct LayerTile
 {
-	int x = 0;
-	int y = 0;
+	int x = 0; //!< X position in the world.
+	int y = 0; //!< Y position in the world.
 
-	int width = 32;
-	int height = 32;
+	int width = 32; //!< The width of the tile
+	int height = 32; //!< The height of the tile
 
 	//Texture
 	int frame = 0; //!< For tile mapping
@@ -20,38 +20,88 @@ struct LayerTile
 
 struct RenderLayer
 {
-	int id;
-	int layer;
-	std::vector<LayerTile*> tiles;
-	std::vector<RenderLayer*> tileLayer;
-	void (* Draw)() = nullptr;
-	bool(*Sort)() = nullptr;
+	int id; //!< ID of this layer
+	int layer; //!< The layer number. Think of this as render order. The higher the number the later it is rendered.
+	std::vector<LayerTile*> tiles; //!< What tiles are in this layer
+	std::vector<RenderLayer*> tileLayer; //!< Additional layers withing this layer. \warning Not used at the moment.
+	void (* Draw)() = nullptr; //!< Function pointer to a externaly defined draw function. (Lets you do things that I didn't think of)
+	bool(*Sort)() = nullptr; //!< Function pointer to a externaly defined sort function. Used to sort out the RenderLayer order according to layer by defult.
 	int data; // not used for anything.
-	int refCount = 0;
+	int refCount = 0; // not used.
 
 };
 
 class RenderEngine {
 public:
+
+	/**
+	 * \brief Construct a new RenderEngine object
+	 * 
+	 */
+
 	RenderEngine();
+
+	/**
+	 * \brief Destroy the RenderEngine object
+	 * 
+	 */
+
 	~RenderEngine();
 
+	/**
+	 * \brief Sets the GameEngine object thats used for drawing.
+	 * 
+	 * \param ge Pointer to a GameEngine object.
+	 * \return true if GameEngine is valid else false.
+	 */
+
 	static bool InitRenderImage(GameEngine * ge);
+	
+	/**
+	 * \brief Adds a layer to the render.
+	 * 
+	 * \param layer The layer to add.
+	 * \see RenderLayer
+	 */
 
 	void AddLayer(RenderLayer * layer);
 
+	/**
+	 * \brief Remove the given layer.
+	 * 
+	 * \param id The ID of the layer to remove.
+	 */
+
 	void RemoveLayer(const int &id);
+
+	/**
+	 * \brief Updates the layers.
+	 * 
+	 *  Removes any nullptrs and sorts the layers into order using Sort.
+	 * 
+	 */
 
 	void Update();
 
+	/**
+	 * \brief Draws all the layers.
+	 * 
+	 */
+
 	void DrawLayers();
+
+	/**
+	 * \brief Draws a single layer.
+	 * 
+	 * \param layer The layer to be drawn.
+	 */
 
 	void DrawLayer(RenderLayer * layer);
 
 
 private:
 
-	std::vector<RenderLayer*> m_layers;
-	static GameEngine * p_gameEngine;
+	std::vector<RenderLayer*> m_layers; //!< Layers to be drawn.
+	static GameEngine * p_gameEngine; //!< Pointer to the GameEngine that is used.
 
 };
