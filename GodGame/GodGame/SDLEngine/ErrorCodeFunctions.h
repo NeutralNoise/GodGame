3ba7badf3b448a9ffaa7cf48183302fa720d59cf
NeutralNoise <classicbrad24@gmail.com>
2019-06-11 16:03:53 +1000

    Added onAdd callback for code 150.

--- GodGame/GodGame/SDLEngine/ErrorCodeFunctions.h
+++ GodGame/GodGame/SDLEngine/ErrorCodeFunctions.h
@@ -2,10 +2,14 @@
 #include <string>
 #include "ErrorEngine.h"
 
-inline std::string Err_150_FontFail(const EngineErrorMessage & err) {
-	return std::string("SDL2_TTF Failed to load font file.\n SDL2_TTF error: " + err.msg);
+inline EngineErrorMessage Err_150_FontFail_onAdd(const std::string & msg) {
+	return EngineErrorMessage(150, EngineErrorTypes::ERR_TYPE_FATEL, msg);
+}
+
+inline std::string Err_150_FontFail_strFunc(const EngineErrorMessage & err) {
+	return std::string("SDL2_TTF Failed to load font file.\nSDL2_TTF error: " + err.msg);
 }
 
 inline std::string Err_151_FontRenderFail(const EngineErrorMessage & err) {
-	return std::string("SDL2_TTF failed to render text to texture\n SDL2_TTF error: " + err.msg);
+	return std::string("SDL2_TTF failed to render text to texture\nSDL2_TTF error: " + err.msg);
 }
