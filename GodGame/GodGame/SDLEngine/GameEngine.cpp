3f88dcd88c567612a85c276a4fc813c3b906fa7e
NeutralNoise <classicbrad24@gmail.com>
2019-06-18 00:49:20 +1000

    Added type punning for conversion to SDL2 rect and removed unused code.

--- GodGame/GodGame/SDLEngine/GameEngine.cpp
+++ GodGame/GodGame/SDLEngine/GameEngine.cpp
@@ -88,31 +88,16 @@ void GameEngine::RenderCopy(Texture * tex, const Rect *rect, const Rect *winRect
 
 	//Convert a engine rect to an SDL2 Rect
 	auto RectToSDLRect = [](Rect r){
-		SDL_Rect rtn;
-		rtn.x = r.x;
-		rtn.y = r.y;
-		rtn.w = r.width;
-		rtn.h = r.height;
+		//Some type punning
+		SDL_Rect rtn = *(SDL_Rect*)&r;
 		return rtn;
 	};
 
 	SDL_Rect sdlrect;
 	if (rect) {
-		/*
-		sdlrect.x = rect->x;
-		sdlrect.y = rect->y;
-		sdlrect.w = rect->width;
-		sdlrect.h = rect->height;
-		*/
 		sdlrect = RectToSDLRect(*rect);
 		if (winRect) {
 			SDL_Rect sdlwinRect;
-			/*
-			sdlwinRect.x = winRect->x;
-			sdlwinRect.y = winRect->y;
-			sdlwinRect.w = winRect->width;
-			sdlwinRect.h = winRect->height;
-			*/
 			sdlwinRect = RectToSDLRect(*winRect);
 			SDL_RenderCopy(p_renderer->renderer, tex->texture, &sdlrect, &sdlwinRect);
 		}
@@ -123,12 +108,6 @@ void GameEngine::RenderCopy(Texture * tex, const Rect *rect, const Rect *winRect
 	else {
 		if (winRect) {
 			SDL_Rect sdlwinRect;
-			/*
-			sdlwinRect.x = winRect->x;
-			sdlwinRect.y = winRect->y;
-			sdlwinRect.w = winRect->width;
-			sdlwinRect.h = winRect->height;
-			*/
 			sdlwinRect = RectToSDLRect(*winRect);
 			SDL_RenderCopy(p_renderer->renderer, tex->texture, NULL, &sdlwinRect);
 		}
