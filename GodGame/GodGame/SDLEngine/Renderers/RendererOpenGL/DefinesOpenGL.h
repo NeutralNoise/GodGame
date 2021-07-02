6fe7417953a792100c0001d143d90288608041a4
NeutralNoise <classicbrad24@gmail.com>
2021-07-02 20:12:22 +1000

    Added defines that are used for OpenGL.

---
+++ GodGame/GodGame/SDLEngine/Renderers/RendererOpenGL/DefinesOpenGL.h
@@ -0,0 +1,8 @@
+#ifndef DEFINES_OPEN_GL_H_INCLUDED
+#define DEFINES_OPEN_GL_H_INCLUDED
+
+//#define ROTATE_AROUND_CENTER //!< Rotate around the center of a RenderObject.
+#define ROTATE_AROUND_TOP_LEFT //!< Rotate around the top left of a RenderObject. \note This is how SDL2 does it.
+
+
+#endif //DEFINES_OPEN_GL_H_INCLUDED
